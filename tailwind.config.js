ac55fe80b5f6eee9256676187c43134de7196bd9
Adesina Adebisi <104420789+AdebisiRex@users.noreply.github.com>
2022-11-25 07:51:13 +0100

    file structure commit, dependencies installed

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+    // './node_modules/tw-elements/dist/js/**/*.js', 
+  ],
+  theme: {
+    extend: {
+      fontFamily:{
+        "primary": 'Inter',
+        "header": 'Outfit',
+      },
+      colors:{
+        lightBlue: "#F5F7FB", 
+        strongBlue: "#0058DB",
+        dark: "#2A2A2A"
+      }
+    },
+  },
+  plugins: [],
+}
