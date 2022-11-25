0fc80529f4203fd280a1be1c7e358784836fcc81
HaroldObasi <haroldobasi2k16@gmail.com>
2022-11-25 11:06:59 +0100

    added table

--- src/pages/CountryInfo.jsx
+++ src/pages/CountryInfo.jsx
@@ -21,6 +21,41 @@ const CountryInfo = () => {
                     <option value="Armenia">Armenia</option>
                     <option value="Argentina">Argentina</option>
                 </select>
+
+                <div className="overflow-x-auto relative mt-5">
+                    <table className="border-collapse w-full text-base text-dark text-left">
+                        <thead className="bg-transparent">
+                            <tr>
+                                <th scope="col" className="py-3 px-6">
+                                    Date
+                                </th>
+                                <th scope="col" className="py-3 px-6">
+                                    Day
+                                </th>
+                                <th scope="col" className="py-3 px-6">
+                                    Name
+                                </th>
+                                <th scope="col" className="py-3 px-6">
+                                    Type
+                                </th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr className="p-5 bg-white my-5">
+                                <td className="py-4 px-6">20 March</td>
+                                <td className="py-4 px-6">Saturday</td>
+                                <td className="py-4 px-6">March Equinox</td>
+                                <td className="py-4 px-6">Season</td>
+                            </tr>
+                            <tr className="p-5 bg-white my-5">
+                                <td className="py-4 px-6">20 March</td>
+                                <td className="py-4 px-6">Saturday</td>
+                                <td className="py-4 px-6">March Equinox</td>
+                                <td className="py-4 px-6">Season</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
             </div>
         </section>
     );
