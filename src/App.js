222f5062824a9c42a90d5db2369f60762a592404
HaroldObasi <haroldobasi2k16@gmail.com>
2022-11-25 15:44:12 +0100

    added navbar to root

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import ScrollToTop from "./components/ScrollToTop";
+import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import CountryInfo from "./pages/CountryInfo";
 
 function App() {
     return (
-        <Router>
-            <ScrollToTop>
-                <Routes>
-                    {/* place all your routes and pages under here */}
-                    <Route path="/" element={<Home />} />
-                    <Route path="/country" element={<CountryInfo />} />
-                </Routes>
-            </ScrollToTop>
-        </Router>
+        <>
+            <Navbar />
+            <Router>
+                <ScrollToTop>
+                    <Routes>
+                        {/* place all your routes and pages under here */}
+                        <Route path="/" element={<Home />} />
+                        <Route path="/country" element={<CountryInfo />} />
+                    </Routes>
+                </ScrollToTop>
+            </Router>
+        </>
     );
 }
 
